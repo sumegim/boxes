45f6393ed1606a5f6fabcfe7eff6b09c872482a9
sumegim <sumegi.mark@gmail.com>
2025-03-16 20:29:25 +0100

    restrict player button hold movement

--- modules/EventHandlers.js
+++ modules/EventHandlers.js
@@ -1,22 +1,31 @@
 export function setupEventHandlers(moveMesh) {
+    const keyState = {};
+
     window.addEventListener("keydown", (event) => {
-        switch (event.key) {
-            case 'w':
-            case 'W':
-                moveMesh('up');
-                break;
-            case 's':
-            case 'S':
-                moveMesh('down');
-                break;
-            case 'a':
-            case 'A':
-                moveMesh('left');
-                break;
-            case 'd':
-            case 'D':
-                moveMesh('right');
-                break;
+        if (!keyState[event.key]) {
+            keyState[event.key] = true;
+            switch (event.key) {
+                case 'w':
+                case 'W':
+                    moveMesh('up');
+                    break;
+                case 's':
+                case 'S':
+                    moveMesh('down');
+                    break;
+                case 'a':
+                case 'A':
+                    moveMesh('left');
+                    break;
+                case 'd':
+                case 'D':
+                    moveMesh('right');
+                    break;
+            }
         }
+    });
+
+    window.addEventListener("keyup", (event) => {
+        keyState[event.key] = false;
     });
 }
